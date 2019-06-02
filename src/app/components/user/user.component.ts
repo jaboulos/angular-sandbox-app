80fc224968af4d7e495d2a75be26bdc38f2609ee
root <root@LAPTOP-FVS3KA6I.localdomain>
2019-06-02 13:50:00 -0700

    properties initially have values attached to them

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -11,10 +11,43 @@ export class UserComponent implements OnInit {
   firstName: string = 'John';
   lastName: string = 'Doe';
   age: number = 30;
+  address = {
+    street: '50 main st',
+    city: 'Boston',
+    state: 'MA'
+  }
 
   // METHODS
   // runs when app is initialized
-  constructor() { }
+  constructor() {
+    this.sayHello();
+    this.changeAddress();
+   }
+
+   sayHello() {
+    //  console.log(`Hello ${this.firstName} ${this.lastName}`)
+    console.log('initial age: ', this.age);
+    this.hasBirthday();
+    console.log('age after method updating the age is put in the constructor: ', this.age);
+
+    this.changeName();
+   }
+
+   hasBirthday() {
+     this.age += 1;
+   }
+
+   changeName() {
+     this.firstName = 'Joe';
+     this.lastName = "Boulos"
+   }
+
+   changeAddress() {
+     this.address.street = "1234 my st"
+     this.address.city = 'Folsom'
+     this.address.state = "CA"
+   }
+
 
   // LIFE CYCLE HOOK
   ngOnInit() {
