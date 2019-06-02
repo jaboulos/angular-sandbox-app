a5000dd5660fa59f28ab998286868369caf73854
root <root@LAPTOP-FVS3KA6I.localdomain>
2019-06-02 13:20:53 -0700

    added some notes

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,12 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  // PROPERTIES
   firstName: string = 'John';
   lastName: string = 'Doe';
   age: number = 30;
 
+  // METHODS
+  // runs when app is initialized
   constructor() { }
 
+  // LIFE CYCLE HOOK
   ngOnInit() {
   }
 
