92cca2de5a3fa78bfcf6469046a43947684ad4ac
Joe Boulos <jab12288@gmail.com>
2019-06-09 18:34:54 -0700

    Get, Post request to API, event emitter and output, communicating between components

--- src/app/Models/Post.ts
+++ src/app/Models/Post.ts
@@ -1,6 +1,13 @@
+// export interface Post {
+//   // check backend fields from database and make this model match it
+//   id: number,
+//   title: string,
+//   body: string
+// }
+
 export interface Post {
   // check backend fields from database and make this model match it
-  id: number,
-  title: string,
-  body: string
+  id: number;
+  title: string;
+  body: string;
 }
--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+// bring in service for making http requests
+import { PostService } from '../../services/post.service';
+
+// bring in model for each post
+import { Post } from '../../Models/Post';
 
 @Component({
   selector: 'app-post-form',
@@ -6,18 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  post: Post;
+  // emmit the new post, that is returned in the subscribe method to oru Posts component
+  @Output() newPost: EventEmitter<Post> = new EventEmitter();
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
   }
 
   // create mehtod to send post request
   addPost(title, body) {
-    if(!title || !body) {
+    if (!title || !body) {
       alert('Please add post');
     } else {
-      console.log(123);
+      // pass in object with fields you want to save in the savePost method, include "as Post" at the end
+      // subscribe, this returns an observable (like a promise)
+      this.postService.savePost({title, body} as Post).subscribe(post => {
+        // emmit the new post, that is returned in the subscribe method to our Posts component
+        // we're emitting an event from post-form component and it includes the new post
+        this.newPost.emit(post);
+        console.log(post);
+      });
     }
   }
 }
