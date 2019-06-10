aba30777c5d3651d9baf624882151a75b779545c
Joe Boulos <jab12288@gmail.com>
2019-06-09 17:06:34 -0700

    added HttpModule and new services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +10,9 @@ import { UserComponent } from './components/user/user.component';
 import { NextUserComponent } from './components/next-user/next-user.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { DataService } from './services/data.service';
+import { UserService } from './services/user.service';
+import { PostService } from './services/post.service';
+import { PostsComponent } from './components/posts/posts.component';
 
 
 @NgModule({
@@ -17,14 +21,16 @@ import { DataService } from './services/data.service';
     UserComponent,
     NextUserComponent,
     UsersComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [DataService],
+  providers: [UserService, PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
