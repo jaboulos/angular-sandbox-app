17d0acfcdc7cf954e1cf7ad4fb9c4119905fd827
Joseph Boulos <joeboulos@joes-mbp.lan>
2019-06-17 07:42:12 -0700

    passes data between sibling components, changes navBar links with ngIf condition for boolean

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
+// import service for login
 import { LoginService } from './../login/login.service';
+// import loggedInUser model
+import { LoggedInUserEventArgs } from './../login/login.component';
+
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +14,23 @@ import { LoginService } from './../login/login.service';
 export class NavbarComponent implements OnInit {
 
   // inject login service
-  constructor(private LoginService: LoginService) { }
+  constructor(private loginService: LoginService) { }
+
+  isUserLogged = false;
+  user: LoggedInUserEventArgs;
 
   ngOnInit() {
     // $isLoggedIn is the variable publishing the user data, need to subscribe to it
-    this.LoginService.$isLoggedIn
+    this.loginService.$isLoggedIn
       // fetch the data
       .subscribe((data) => {
-        console.log('I got this data', data)
-      })
+        console.log('I got this data', data);
+
+        // this.isUserLogged = true;
+        this.isUserLogged = !this.isUserLogged;
+        // user data
+        this.user = data;
+      });
   }
 
 }
